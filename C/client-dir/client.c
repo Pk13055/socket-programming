fcaf2169f04c5014574f6078549a62c16f620e7f
AdLucem <ghosalatreyee@gmail.com>
2018-01-22 01:40:48 +0530

    transfer of multiple files not working

--- C/client-dir/client.c
+++ C/client-dir/client.c
@@ -19,43 +19,47 @@ typedef int bool;
 
 int main(int argc, char const *argv[])
 {
-    struct sockaddr_in address;
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-    char *filename = "file1.txt";
-    char buffer[5 * PACKET_SIZE] = {NULL};
-
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  struct sockaddr_in address;
+  int sock = 0, valread;
+  struct sockaddr_in serv_addr;
+  char *filename[] = {"file1.txt", "file2.txt"};
+  char buffer[5 * PACKET_SIZE] = {NULL};
+  
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("\n Socket creation error \n");
-        return -1;
+      printf("\n Socket creation error \n");
+      return -1;
     }
 
-    memset(&serv_addr, '0', sizeof(serv_addr)); // to make sure the struct is empty. Essentially sets sin_zero as 0
-                                                // which is meant to be, and rest is defined below
+  memset(&serv_addr, '0', sizeof(serv_addr)); // to make sure the struct is empty. Essentially sets sin_zero as 0
+  // which is meant to be, and rest is defined below
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons((argc >= 2)? atoi(argv[1]) : PORT);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons((argc >= 2)? atoi(argv[1]) : PORT);
 
-    // Converts an IP address in numbers-and-dots notation into either a
-    // struct in_addr or a struct in6_addr depending on whether you specify AF_INET or AF_INET6.
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+  // Converts an IP address in numbers-and-dots notation into either a
+  // struct in_addr or a struct in6_addr depending on whether you specify AF_INET or AF_INET6.
+  if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
     {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
+      printf("\nInvalid address/ Address not supported \n");
+      return -1;
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)  // connect to the server address
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)  // connect to the server address
     {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
+      printf("\nConnection Failed \n");
+      return -1;
+    } 
 
-    send(sock , filename , strlen(filename) , 0 );  // send the message.
+  int no_of_files = 1, i;
+
+  for (i = 0; i < no_of_files; i++) {
+
+    send(sock , filename[i] , strlen(filename[i]) , 0 );  // send the message.
     printf("File name sent\n");
 
     // open the sent file ready for receiving
-    int fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0644);
+    int fd = open(filename[i], O_CREAT | O_WRONLY | O_APPEND, 0644);
 
     // error handling for opening the file
     if ( fd < 0 ) {
@@ -63,14 +67,22 @@ int main(int argc, char const *argv[])
       return 1;
     }
 
+
     int   current_received = 0;
     bool first_run = true;
     // start IO loop to gather the file packets
     while(1) {
 
+      // test print
+      printf("READING: ");
       // receive message back from server, into the buffer
       valread = read(sock , buffer, PACKET_SIZE);
 
+
+      // test print
+      printf(" %d bytes\n",valread);
+
+      // test print
       if (valread < 0) {
         perror("Unable to receive contents.");
         return 1;
@@ -85,10 +97,14 @@ int main(int argc, char const *argv[])
       first_run = false;
       current_received += valread;
 
-      printf("%s\n",buffer );
       write(fd, buffer, valread);
     }
+
     printf("Total bytes received: %d\n", current_received);
     close(fd);
-    return 0;
+  
+  }
+
+  send(sock , "0" , 1 , 0 );  // send the ending messgae
+  return 0;
 }
--- C/server-dir/server.c
+++ C/server-dir/server.c
@@ -16,91 +16,97 @@
 
 int main(int argc, char const *argv[])
 {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;  // sockaddr_in - references elements of the socket address. "in" for internet
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
+  int server_fd, new_socket, valread;
+  struct sockaddr_in address;  // sockaddr_in - references elements of the socket address. "in" for internet
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[1024] = {0};
+  char *hello = "Hello from server";
 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)  // creates socket, SOCK_STREAM is for TCP. SOCK_DGRAM for UDP
+  // Creating socket file descriptor
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)  // creates socket, SOCK_STREAM is for TCP. SOCK_DGRAM for UDP
     {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+      perror("socket failed");
+      exit(EXIT_FAILURE);
     }
 
-    // This is to lose the pesky "Address already in use" error message
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt))) // SOL_SOCKET is the socket layer itself
+  // This is to lose the pesky "Address already in use" error message
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                 &opt, sizeof(opt))) // SOL_SOCKET is the socket layer itself
     {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+      perror("setsockopt");
+      exit(EXIT_FAILURE);
     }
 
-    address.sin_family = AF_INET;  // Address family. For IPv6, it's AF_INET6. 29 others exist like AF_UNIX etc.
-    address.sin_addr.s_addr = INADDR_ANY;  // Accept connections from any IP address - listens from all interfaces.
-    address.sin_port = htons( (argc >= 2)? atoi(argv[1]) : PORT );    // Server port to open. Htons converts to Big Endian - Left to Right. RTL is Little Endian
+  address.sin_family = AF_INET;  // Address family. For IPv6, it's AF_INET6. 29 others exist like AF_UNIX etc.
+  address.sin_addr.s_addr = INADDR_ANY;  // Accept connections from any IP address - listens from all interfaces.
+  address.sin_port = htons( (argc >= 2)? atoi(argv[1]) : PORT );    // Server port to open. Htons converts to Big Endian - Left to Right. RTL is Little Endian
 
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address,
-       sizeof(address))<0)
+  // Forcefully attaching socket to the port 8080
+  if (bind(server_fd, (struct sockaddr *)&address,
+           sizeof(address))<0)
     {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+      perror("bind failed");
+
+      exit(EXIT_FAILURE);
     }
 
-    // Port bind is done. You want to wait for incoming connections and handle them in some way.
-    // The process is two step: first you listen(), then you accept()
-    if (listen(server_fd, 3) < 0) // 3 is the maximum size of queue - connections you haven't accepted
+  // Port bind is done. You want to wait for incoming connections and handle them in some way.
+  // The process is two step: first you listen(), then you accept()
+  if (listen(server_fd, 3) < 0) // 3 is the maximum size of queue - connections you haven't accepted
     {
-        perror("listen");
-        exit(EXIT_FAILURE);
+      perror("listen");
+      exit(EXIT_FAILURE);
     }
 
-    // returns a brand new socket file descriptor to use for this single accepted connection. Once done, use send and recv
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-     (socklen_t*)&addrlen))<0)
+  // returns a brand new socket file descriptor to use for this single accepted connection. Once done, use send and recv
+  if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                           (socklen_t*)&addrlen))<0)
     {
-        perror("accept");
-        exit(EXIT_FAILURE);
+      perror("accept");
+      exit(EXIT_FAILURE);
     }
 
-    valread = read( new_socket , buffer, 1024);  // read infromation received into the buffer
-    printf("File to transfer: %s\n",buffer );
+  valread = read( new_socket , buffer, 1024);  // read infromation received into the buffer
 
-    // open file to be transferred
-    int fd;
-    fd = open(buffer, O_RDONLY);
+  //  if ( strcmp(buffer, "0") == 0) {
+  //break;
+  //}
 
-    // error handling for opening the file
-    if (fd < 0) {
-      perror("File failed to open.");
-      return 1;
+  printf("File to transfer: %s\n",buffer );
+
+  // open file to be transferred
+  int fd;
+  fd = open(buffer, O_RDONLY);
+
+  // error handling for opening the file
+  if (fd < 0) {
+    perror("File failed to open.");
+    return 1;
   }
 
   while(1) {
 
-      char read_buffer[5 * PACKET_SIZE] = {NULL};
-      // try reading PACKET_SIZE characters
-      // from the file
-      int bytes_read = read(fd, read_buffer, PACKET_SIZE);
+    char read_buffer[5 * PACKET_SIZE] = {NULL};
+    // try reading PACKET_SIZE characters
+    // from the file
+    int bytes_read = read(fd, read_buffer, PACKET_SIZE);
 
-      if (bytes_read < 0) {
-        perror("Unable to read from file.");
-        return 1;
+    if (bytes_read < 0) {
+      perror("Unable to read from file.");
+      return 1;
     }
-      else if (bytes_read == 0) {
-        // signal to end the transfer
-        // socket, buffer, length, flags
-        printf("File sent successfully\n");
-        send(new_socket , NULL , 1, 0 );
-        break;
+    else if (bytes_read == 0) {
+      // signal to end the transfer
+      // socket, buffer, length, flags
+      printf("File sent successfully\n");
+      send(new_socket , NULL , 1, 0 );
+      break;
     }
 
     printf("%d bytes sent\n", bytes_read);
     send(new_socket , read_buffer , strlen(read_buffer) , 0 );
-}
+  }
 
-return 0;
+  return 0;
 }
